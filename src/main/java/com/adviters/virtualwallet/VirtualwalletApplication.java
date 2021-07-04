73381e59ac25e3b0f77c57a68fefe20f3507b8f8
marianoponte <mariano.ponte@hotmail.com>
2021-07-04 20:40:18 -0300

    primer commit

---
+++ src/main/java/com/adviters/virtualwallet/VirtualwalletApplication.java
@@ -0,0 +1,13 @@
+package com.adviters.virtualwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualwalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualwalletApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adviters/virtualwallet/VirtualwalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adviters.virtualwallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualwalletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
